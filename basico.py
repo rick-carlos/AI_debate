f02f2396ef3ddae6a4e9557ac2c230c7b93a0721
kinzey <chcavil@gmail.com>
2025-04-14 20:57:33 -0300

    consertado bug onde os novos diálogos não estavam sendo salvos no contexto

--- basico.py
+++ basico.py
@@ -1,6 +1,8 @@
 import ollama
 
 assistente = "gemma3:latest"
+contexto = []  # criado a lista de contexto fora do loop while pra ele não ser
+               # recarregado toda vez.
 
 print(f"\n {10 * '*'} Assistente: Olá, sobre o que você gostaria de conversar? {10 * '*'} \n")
 print(f"{10 * '-'} Digite /bye pra sair do chat {10 * '-'} \n")
@@ -16,23 +18,26 @@ while True:
 
     else:
         
-        contexto = [{'role': 'user', 'content': pergunta}]
+        contexto.append({'role': 'user', 'content': pergunta})
         
-        # stream=True pra printar conforme gera as palavras
+        # stream =True pra printar conforme gera as palavras
         resposta = ollama.chat(model=assistente, messages=contexto, stream=True) 
         
         resposta_completa = ''
 
         for parte in resposta:
-            chat = parte['message']['content']
-            
-            resposta_completa += chat
+            if 'message' in parte: # necessário if por conta do texto inserido em resposta_completa abaixo
+                chat = parte['message']['content']
+                
+                # o modelo vai gerar a resposta e o thinking como metadado usado que será aninhado no output
+                # o thinking é armazenado em um dicionário próprio como internal thoughts ou system messages 
+                # mas no stream convertido em texto, thinking será armazenado no reposta_completa tambem
+                # use if pra destacar apenas os textos no dicionário 'message' e ignorar o thinking
+                resposta_completa += chat
 
-            # sem o end='', vai pular a linha pra cada palavra printada
-            print(chat, end='', flush=True) # flush pra usar o stream
+                # sem o end='', vai pular a linha pra cada palavra printada
+                print(chat, end='', flush=True) # flush pra usar o stream
         
-        contexto.append([{'role': 'assistant', 'content': resposta_completa}])
-        
-        
+        contexto.append({'role': 'assistant', 'content': resposta_completa})
 
 print(f"\n {10 * '*'} Assistente: Adeus {10 * '*'} \n")
